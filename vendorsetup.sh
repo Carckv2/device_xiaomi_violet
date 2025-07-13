0fb6b28b480021161df92fbfc1468c54f4308aad
Your Name <you@example.com>
2025-07-13 12:43:34 -0700

    violet: some_modify_A10

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,8 @@
 # Vendor tree
-git clonehttps://github.com/PixelExperience-Blobs/vendor_xiaomi_violet.git -b ten vendor/xiaomi/violet
+git clone https://github.com/PixelExperience-Blobs/vendor_xiaomi_violet.git -b ten vendor/xiaomi/violet
 
 # Kernel tree
-git clone https://github.com/PixelExperience-Devices/kernel_xiaomi_violet.git -b ten vendor/xiaomi/violet
+git clone --depth=1 https://github.com/PixelExperience-Devices/kernel_xiaomi_violet.git -b ten kernel/xiaomi/violet
 
 # clang
 git clone --depth=1 https://github.com/DrtSinX98/android_prebuilts_clang_host_linux-x86_clang-6443078 prebuilts/clang/host/linux-x86/clang-6443078
