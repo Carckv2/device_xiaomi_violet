cb4256393d87f8a824180cfd9e74b5d6c3c46bf3
Your Name <you@example.com>
2025-07-14 10:07:29 -0700

    violet: missing_added :)

--- device.mk
+++ device.mk
@@ -5,6 +5,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Signing
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/releasekey
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,22 +2,25 @@
 git clone -b master https://github.com/karthik558/vendor_ANXCamera vendor/aeonax/ANXCamera
 
 # Vendor tree
-rm -rf vendor/xiaomi/violet
 git clone https://github.com/PixelExperience-Blobs/vendor_xiaomi_violet.git -b ten vendor/xiaomi/violet 
 
 # Firmware
-rm -rf vendor/xiaom-firmware/violet
 git clone git@github.com:Carckv2/Vendor_Xiaomi-Firmware_Violet.git -b ten vendor/xiaomi-firmware/violet
 
 # Kernel tree
-rm -rf   kernel/xiaomi/violet
 git clone --depth=1 https://github.com/PixelExperience-Devices/kernel_xiaomi_violet.git -b ten kernel/xiaomi/violet
 
 # clang
-rm -rf prebuilts/clang/host/linux-x86/clang-r498229
 git clone https://gitlab.com/Joker-V2/prebuilts_clang_host_linux-x86_clang-r498229.git prebuilts/clang/host/linux-x86/clang-r498229 --depth=1
+
+#  hardware/xiaomi
+git clone -b lineage-17.1 https://github.com/LineageOS/android_hardware_xiaomi.git -b ten hardware/xiaomi
+
+#  vendor/lineage-priv/keys
+git clone -b 10 https://github.com/Carckv2/opengapps.git vendor/lineage-priv/keys
+
+#  packages/apps/PixelLiveWallpaper
+git clone -b ten https://gitlab.com/PixelExperience/packages_apps_PixelLiveWallpaper.git packages/apps/PixelLiveWallpaper
 
 export KBUILD_BUILD_HOST="nekokawai"
 export KBUILD_BUILD_USER="ninja"
-
-# VT-KT-Taken-From-Pixel-Experience
